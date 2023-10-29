52b95e77b2cfbc3c18fbc8d5094cafb97c308955
Nadzeya Dzeranhouskaya <Nadzeya.Dzeranhouskaya@reedonline.co.uk>
2023-10-29 16:46:02 +0400

    add components

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-const UsersPage = () => {
+interface User { // create interface for fetch data
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const result = await fetch('https://jsonplaceholder.typicode.com/users'); // make request
+  const users: User[] = await result.json(); // get data
+
   return (
-    <div>UsersPage</div>
+    <>
+      <h1>Users</h1>
+      <ul>
+        {/* display data */}
+        {users.map(user=><li key={user.id}>{user.name}</li>)}
+      </ul>
+    </>
+
   )
 }
 
